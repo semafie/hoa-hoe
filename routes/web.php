5d9a6c25ad486cbf5262582e881323ac1c935edc
ningfaiqohalfaqorina <ningfaiqoh95@gmail.com>
2024-12-26 14:05:54 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('leanding_page.home');
 });
+
+Route::get('/login', function () {
+    return view('login_register.login');
+});
